d84dbefd03989f9cedce07a1a379a69c52e392ab
Aleksandr Rybolovlev <asr@netcow.org>
2022-05-25 15:36:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
 variable "string_length" {
   default = 8
 }
-
-variable "tfc_org_name" {
-  description = "Terraform Cloud Platform Organisation Name"
-  type        = string
-  default     = ""
-}
-
-variable "tfc_token" {
-  description = "Terraform Cloud Platform Token"
-  type        = string
-  default     = ""
-}
