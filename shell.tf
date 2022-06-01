cfe7b33d05f748aaecae1e9ce3a6dd4032bc58f6
Aleksandr Rybolovlev <asr@netcow.org>
2022-06-01 10:40:18 +0200

    Update shell.tf

--- shell.tf
+++ shell.tf
@@ -1,13 +1,3 @@
-resource "null_resource" "dns" {
-	triggers = {
-		now = timestamp()
-	}
-
-	provisioner "local-exec" {
-		command = "sudo echo 'nameserver 1.1.1.1' > /etc/resolv.conf"
-	}
-}
-
 resource "null_resource" "resolv" {
 	triggers = {
 		now = timestamp()
