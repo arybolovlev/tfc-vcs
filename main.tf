536f60c86a7b49f3dda361e5466856eb8e2089b3
Aleksandr Rybolovlev <asr@netcow.org>
2022-08-15 09:12:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ resource "random_string" "this" {
   special = false
   upper   = true
 }
+
+variable "remote_state" {
+  default = false
+}
+
+data "terraform_remote_state" "this" {
+  count = var.remote_state ? 1 : 0
+  backend = "remote"
+
+  config = {
+    organization = "strategic-integrations"
+    workspaces = {
+      name = "rybolovlev-workspace"
+    }
+  }
+}
+
+output "remote_state" {
+  value = data.terraform_remote_state.this
+}
