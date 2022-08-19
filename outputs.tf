b8c4810db32a8276f8e46445bed32ee46f555312
Aleksandr Rybolovlev <asr@netcow.org>
2022-08-19 15:28:57 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,49 @@
-output "random_string" {
-  value = random_string.this.*.result
-}
-
-output "object" {
-  value = {
-    "a" = "1"
-    "b" = "2"
-    "c" = [
-      3.1,
-      3.2,
-      3.3,
-    ]
-  }
-}
-
-output "map" {
-  value = {
-    "a" = "1"
-    "b" = "2"
-    "c" = "3"
-  }
-}
-
-output "bool" {
-  value = false
-}
-
-output "list" {
-  value = [1, 2, 4, 8, 16, 32]
-}
-
-output "counter" {
-  value = var.counter
-}
-
-output "json" {
-  value = var.json
-}
-
-output "secret" {
-  value = "s3kr3tPassword007"
-  sensitive = false
-}
-
-output "string_length" {
-  value = var.string_length
-}
+# output "random_string" {
+#     value = random_string.this.*.result
+#   }
+  
+#   output "object" {
+#     value = {
+#       "a" = "1"
+#       "b" = "2"
+#       "c" = [
+#         3.1,
+#         3.2,
+#         3.3,
+#       ]
+#     }
+#   }
+  
+#   output "map" {
+#     value = {
+#       "a" = "1"
+#       "b" = "2"
+#       "c" = "3"
+#     }
+#   }
+  
+#   output "bool" {
+#     value = false
+#   }
+  
+#   output "list" {
+#     value = [1, 2, 4, 8, 16, 32]
+#   }
+  
+#   output "counter" {
+#     value = var.counter
+#   }
+  
+#   output "json" {
+#     value = var.json
+#   }
+  
+#   output "secret" {
+#     value = "s3kr3tPassword007"
+#     sensitive = false
+#   }
+  
+#   output "string_length" {
+#     value = var.string_length
+#   }
+  
