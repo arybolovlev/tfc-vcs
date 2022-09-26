61970cd80317771f2cb06c8a18e1edce95a21146
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2022-09-26 10:31:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,33 +5,3 @@ resource "time_sleep" "this" {
     random_string.this
   ]
 }
-
-resource "random_string" "this" {
-  count = length(var.counter)
-
-  length   = var.counter[count.index]
-  lower    = true
-  numeric  = true
-  special  = false
-  upper    = true
-}
-
-# variable "remote_state" {
-#   default = false
-# }
-
-# data "terraform_remote_state" "this" {
-#   count = var.remote_state ? 1 : 0
-#   backend = "remote"
-# 
-#   config = {
-#     organization = "strategic-integrations"
-#     workspaces = {
-#       name = "rybolovlev-workspace"
-#     }
-#   }
-# }
-# 
-# output "remote_state" {
-#   value = data.terraform_remote_state.this
-# }
