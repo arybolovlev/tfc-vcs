f2f5107bf0584e6fe16df37a5f4843b2e16194f9
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2022-11-09 20:04:06 +0100

    Update issue.tf

--- issue.tf
+++ issue.tf
@@ -1,22 +1,15 @@
 variable "issue" {
   type = object({
-    name     = string
-    size     = string
-    username = string
-    password = string
-    list     = list(string)
+    string = string
+    list   = list(string)
   })
 
   default = {
-      name     = "example-vm"
-      size     = "Standard_F2"
-      username = "adminuser"
-      password = "Notallowed1!"
-      list     = []
+      string = "text"
+      list   = []
     }
   
 }
-
 
 output "issue" {
   value = var.issue
