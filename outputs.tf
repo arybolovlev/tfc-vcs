e16f9e4fa2e294b0f5af4751187327b3decbaa0c
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2022-11-09 16:00:08 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,27 @@ output "secret" {
 output "random_strings" {
   value = random_string.this.*.result
 }
+
+output "issue" {
+  value = "{
+  "level-a": {
+    "level-b": {
+      "point-a": "value-a",
+      "point-b": "value-b",
+      "point-c": "",
+      "point-d": "value-d",
+      "point-e": "value-e",
+      "id": "JKASKdhkhasdkajs28712",
+      "kubernetes": [
+        {
+          "step-a": "step-a",
+          "step-b": "step-a",
+          "step-c": "step-a"
+        }
+      ],
+      "name-a": "name-value",
+      "name-b": ""
+    }
+  }
+}"
+}
