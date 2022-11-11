ac4b32a4c59ed2730de10260186b2f9beac79ab5
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2022-11-11 15:30:20 +0100

    Update issue.tf

--- issue.tf
+++ issue.tf
@@ -17,6 +17,26 @@ output "issue" {
 
 #####
 
+variable "issue_2" {
+  type = object({
+    string = string
+    list   = list(string)
+  })
+
+  default = {
+      string = "text"
+      list   = ["een", "twee"]
+    }
+  
+}
+
+output "issue_2" {
+  value = var.issue_2
+}
+
+
+#####
+
 variable "no_issue" {
   type = object({
     string = string
