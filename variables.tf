e8152539bdd0fccd2880054c165ea80548251217
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2023-09-06 13:50:30 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
-variable "counter" {
-  default = [
-    1,
-    2,
-  ]
+#variable "counter" {
+#  default = [
+#    1,
+#    2,
+#  ]
+#}
+
+variable "username_length" {
+  default = 1
+}
+
+variable "secret_length" {
+  default = 1
 }
